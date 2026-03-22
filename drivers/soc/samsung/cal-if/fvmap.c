6d2090eb389661dc4d783d494d63ab03617c3085
jsdjawhaa0309 <142013136+jsdjawhaa0309@users.noreply.github.com>
2026-03-22 18:41:32 +0800

    Overclock test

--- drivers/soc/samsung/cal-if/fvmap.c
+++ drivers/soc/samsung/cal-if/fvmap.c
@@ -322,13 +322,13 @@ static void fvmap_copy_from_sram(void __iomem *map_base, void __iomem *sram_base
 
 		for (j = 0; j < fvmap_header[i].num_of_lv; j++) {
 			if (fvmap_header[i].num_of_lv == 20 && fvmap_header[i].num_of_members == 2) { // BIG
-				if (old->table[j].rate == 2496000) {
-					new->table[j].volt = old->table[j].volt + 50000 - 6250;
-				} else if (old->table[j].rate == 2496000) {
-					new->table[j].volt = old->table[j].volt + 75000;
+				if (old->table[j].rate == 2600000) {
+  					new->table[j].volt = old->table[j].volt + 25000;
+				} else if (old->table[j].rate == 2600000) {
+    				new->table[j].volt = old->table[j].volt + 12500;
 				} else {
-					new->table[j].volt = old->table[j].volt;
-				}
+    				new->table[j].volt = old->table[j].volt;
+			}
 			} else if (fvmap_header[i].num_of_lv == 16 && fvmap_header[i].num_of_members == 2) { // LITTLE
                                 if (old->table[j].rate == 1950000 || old->table[j].rate == 2054000) {
                                         new->table[j].volt = old->table[j].volt - 25000;
