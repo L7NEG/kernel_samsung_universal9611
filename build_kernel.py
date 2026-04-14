a5aaed3f479e25f2d65224088becac40fab3a2d9
L7NEG <mahmodealking467@gmail.com>
2026-04-14 11:19:06 +0200

    disabled lto to test if kpatch-next will work or not

--- build_kernel.py
+++ build_kernel.py
@@ -95,8 +95,8 @@ def main():
         print("Please specify one of the following variants: --oneui or --aosp")
         return
     
-    if not args.target in ['a51', 'm21', 'm31', 'm31s', 'f41', 'm30s', 'gta4xl', 'gta4xlwifi']:
-        print("Please specify a valid target: a51/m21/m31/m31s/f41/m30s/gta4xl/gta4xlwifi")
+    if not args.target in ['a51', 'm21', 'm31', 'm31s', 'f41', 'm30s']:
+        print("Please specify a valid target: a51/m21/m31/m31s/f41/m30s")
         return
     
     # Check files
